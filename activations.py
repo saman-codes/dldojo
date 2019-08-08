Fix relu, leaky relu,  gradient checking

activations.py
@@ -20,8 +20,10 @@ class Relu(Activation):
     def __call__(self, x):
         return np.maximum(0, x)
 
-    def derivative(self, x):
-        return self.__call__(x) / x
+    def derivative(self, x): 
+        x[x>=0] = 1
+        x[x<1] = 0
+        return x
 
 
 class LeakyRelu(Activation):
@@ -34,7 +36,9 @@ class LeakyRelu(Activation):
         return np.maximum(self.mu*x, x)
 
     def derivative(self, x):
-        return (np.maximum(0, x) / x) * self.mu
+        x[x>=0] = 1
+        x[x<1] = self.mu
+        return x
 
 
 class Linear(Activation):

layers.py
@@ -110,6 +110,7 @@ class Layer():
             dwx *= self.dropout_mask
         self.error = next_layer.weights.T.dot(next_layer.error) * dwx
         self.gradient = self.error.dot(self.x.T)
+        self.bias_gradient = self.error.sum(axis=1, keepdims=True)
         return
 
     def update_weights(self, learning_rate, batch_size):
@@ -118,7 +119,7 @@ class Layer():
             self.weights -= learning_rate/batch_size * self.gradient
             # Update bias (average error over batches)
             if self.use_bias:
-                self.bias -= learning_rate/batch_size * self.error.sum(axis=1, keepdims=True)
+                self.bias -= learning_rate/batch_size * self.bias_gradient
         return
 
     def _set_dropout_mask(self):

main.py
@@ -26,42 +26,51 @@ def run_autoencoder():
                    use_bias=True, bias_init='zeros', weight_init='normal'))
     net.train(x_train,  x_train, loss, batch_size=bs, learning_rate=5,
               epochs=25, verbose=False, plot_loss=True)
-
-    get_accuracy_mnist(x_train, x_train, net)
     plot_weights(net)
     generator = Network()
     generator.set_name('Generator')
     generator.layers = net.layers[2:]
     plot_random_mnist_autoencoder(generator)
 
-
-def run_feedforward():
-    # x_train, y_train, x_test, y_test = load_mnist(
-    #     train_set_size=10, test_set_size=1)
-    ins = 1
-    os = 2
-    hs = 2
-    bs = 1
-    x_train = np.ones((ins, 1))
-    y_train = np.ones((os, 1))
-    loss = MSE()
-    # loss = CrossEntropy()
+def run_feedforward_gradient_checking():
+    bs = 10
+    ins = 784
+    os = 10
+    hs = 10
+    x_train, y_train, _, _ = load_mnist(train_set_size=bs, test_set_size=0)
+    # loss = MSE()
+    loss = CrossEntropy()
     net = Network()
-    net.set_name('Simple Feedforward Network')
-    net.add(Feedforward(shape=(hs, ins), activation='sigmoid',
+    net.add(Feedforward(shape=(hs, ins), activation='relu',
                         use_bias=True, bias_init='normal', weight_init='normal'))
-    # net.add(Feedforward(shape=(hs, hs), activation='sigmoid',
+    # net.add(Feedforward(shape=(hs, hs), activation='relu',
     #                     use_bias=True, bias_init='normal', weight_init='normal'))
     net.add(Output(shape=(os, hs), activation='sigmoid',
                    use_bias=True, bias_init='normal', weight_init='normal'))
-    # net.train(x_train,  y_train, loss, batch_size=bs, learning_rate=1,
-    #           epochs=100, regularizer=('L2', 0.3), verbose=True, plot_loss=False)
     net.train(x_train,  y_train, loss, gradient_check=True, batch_size=bs, learning_rate=1,
               epochs=1, plot_loss=False)
-    # get_accuracy_mnist(x_test, y_test, net)
-    # plot_weights(net)
-    # predict_random_mnist(x_test, y_test, net, save_plot=True)
 
+def run_feedforward():
+    x_train, y_train, x_test, y_test = load_mnist(train_set_size=10000, test_set_size=1000)
+    ins = 784
+    os = 10
+    hs = 100
+    bs = 1000
+    # loss = MSE()
+    loss = CrossEntropy()
+    net = Network()
+    net.set_name('Simple Feedforward Network')
+    net.add(Feedforward(shape=(hs, ins), activation='relu',
+                        use_bias=True, bias_init='normal', weight_init='normal'))
+    net.add(Feedforward(shape=(hs, hs), activation='relu',
+                        use_bias=True, bias_init='normal', weight_init='normal'))
+    net.add(Output(shape=(os, hs), activation='sigmoid',
+                   use_bias=True, bias_init='normal', weight_init='normal'))
+    net.train(x_train,  y_train, loss, batch_size=bs, learning_rate=1e-3,
+              epochs=100, regularizer=('L2', 0.3), verbose=False, plot_loss=False)
+    get_accuracy_mnist(x_test, y_test, net)
+    plot_weights(net)
+    predict_random_mnist(x_test, y_test, net, save_plot=True)
 
 def run_cnn():
     x_train, y_train, x_test, y_test = load_mnist(
@@ -88,6 +97,7 @@ def run_cnn():
 
 if __name__ == '__main__':
     # run_autoencoder()
-    run_feedforward()
+    # run_feedforward()
+    run_feedforward_gradient_checking()
     # run_cnn()
 

network.py
@@ -85,6 +85,7 @@ class Network():
                         layer.error = self.loss.output_gradient(
                             output, minibatch_y) * dwx
                         layer.gradient = layer.error.dot(layer.x.T)
+                        layer.bias_gradient = layer.error.sum(axis=1, keepdims=True)
                     else:
                         layer.backward(next_layer)
                     if self.regularizer:
@@ -103,7 +104,7 @@ class Network():
 
     def _check_gradient(self, x, y, layer):
         epsilon = 1e-5
-        for i in range(layer.shape[0]):
+        for i in tqdm(range(layer.shape[0])):
             for j in range(layer.shape[1]):
                 layer.weights[i,j] += epsilon
                 output_plus = self.test_predict(x)
@@ -111,22 +112,24 @@ class Network():
                 layer.weights[i,j] -= 2*epsilon
                 output_minus = self.test_predict(x)
                 loss_minus = self.loss(output_minus, y)
+                raw_gradient = ((loss_plus - loss_minus)/(2*epsilon))
                 gradient = ((loss_plus - loss_minus)/(2*epsilon)).sum()
                 backprop_gradient = layer.gradient[i,j]
-                if not np.isclose(backprop_gradient, gradient, rtol=1e-5):
+                if not np.isclose(backprop_gradient, gradient, rtol=1):
                     raise Exception(f"Computed gradient is not correct for layer {layer}")
                 # Reset weights
                 layer.weights[i,j] += epsilon
         if layer.use_bias:
-            for i in range(len(layer.bias)):
+            for i in tqdm(range(len(layer.bias))):
                 layer.bias[i,0] += epsilon
                 output_plus = self.test_predict(x)
                 loss_plus = self.loss(output_plus, y)
                 layer.bias[i,0] -= 2*epsilon
                 output_minus = self.test_predict(x)
                 loss_minus = self.loss(output_minus, y)
+                raw_gradient = ((loss_plus - loss_minus)/(2*epsilon))
                 gradient = ((loss_plus - loss_minus)/(2*epsilon)).sum()
-                backprop_gradient = layer.error[i].sum()
+                backprop_gradient = layer.bias_gradient[i]
                 if not np.isclose(backprop_gradient, gradient, rtol=1e-5):
                     raise Exception(f"Computed gradient is not correct for bias in layer {layer}")
                 layer.bias[i,0] += epsilon