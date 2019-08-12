Fix relu, backprop stuff, add minmax scaling

activations.py
@@ -1,3 +1,6 @@
+# Standard Python
+import copy
+
 # Thirdparty
 import numpy as np
 
@@ -18,12 +21,10 @@ class Activation():
 class Relu(Activation):
 
     def __call__(self, x):
-        return np.maximum(0, x)
+        return np.absolute(x * (x > 0))
 
-    def derivative(self, x): 
-        x[x>=0] = 1
-        x[x<1] = 0
-        return x
+    def derivative(self, x):
+        return np.absolute(1. * (x > 0))
 
 
 class LeakyRelu(Activation):

layers.py
@@ -19,7 +19,8 @@ class Layer():
                  bias_init='zeros',
                  is_trainable=True,
                  dropout=1.,
-                 flatten=False):
+                 flatten=False,
+                 minmax_scaling=False):
 
         self.is_trainable = is_trainable
         self.shape = shape
@@ -27,6 +28,7 @@ class Layer():
         self.add_dropout = bool(dropout < 1.)
         self.dropout_p = dropout
         self.flatten = flatten
+        self.minmax_scaling = minmax_scaling
         self.is_output_layer = False
         '''
         TODO: Init-ing to None for now, change to an np.zeros with correct shape
@@ -92,7 +94,9 @@ class Layer():
         if self.flatten:
             x = x.reshape(-1, 1)
         self.x = x
-        self.wx = self.weights.dot(x)
+        if self.minmax_scaling:
+            self.x = np.divide((x - np.amin(x)),(np.amax(x) - np.amin(x)))
+        self.wx = self.weights.dot(self.x)
         if self.use_bias:
             self.wx += self.bias.dot(np.ones((1,self.wx.shape[1])))
         self.out = self.activation(self.wx)

main.py
@@ -33,11 +33,13 @@ def run_autoencoder():
     plot_random_mnist_autoencoder(generator)
 
 def run_feedforward_gradient_checking():
-    bs = 10
+    bs = 1
     ins = 784
     os = 10
     hs = 10
     x_train, y_train, _, _ = load_mnist(train_set_size=bs, test_set_size=0)
+    # x_train = np.random.random((ins, bs))
+    # y_train = np.ones((ins, bs))*10
     # loss = MSE()
     loss = CrossEntropy()
     net = Network()
@@ -60,17 +62,20 @@ def run_feedforward():
     loss = CrossEntropy()
     net = Network()
     net.set_name('Simple Feedforward Network')
-    net.add(Feedforward(shape=(hs, ins), activation='relu',
-                        use_bias=True, bias_init='normal', weight_init='normal'))
-    net.add(Feedforward(shape=(hs, hs), activation='relu',
-                        use_bias=True, bias_init='normal', weight_init='normal'))
+    net.add(Feedforward(shape=(hs, ins), activation='sigmoid',
+                        use_bias=True, bias_init='normal', weight_init='normal',
+                        minmax_scaling=False))
+    net.add(Feedforward(shape=(hs, hs), activation='sigmoid',
+                        use_bias=True, bias_init='normal', weight_init='normal',
+                        minmax_scaling=False))
     net.add(Output(shape=(os, hs), activation='sigmoid',
-                   use_bias=True, bias_init='normal', weight_init='normal'))
+                   use_bias=True, bias_init='normal', weight_init='normal',
+                   minmax_scaling=False))
     net.train(x_train,  y_train, loss, batch_size=bs, learning_rate=1e-3,
               epochs=100, regularizer=('L2', 0.3), verbose=False, plot_loss=False)
     get_accuracy_mnist(x_test, y_test, net)
-    plot_weights(net)
-    predict_random_mnist(x_test, y_test, net, save_plot=True)
+    # plot_weights(net)
+    # predict_random_mnist(x_test, y_test, net, save_plot=True)
 
 def run_cnn():
     x_train, y_train, x_test, y_test = load_mnist(
@@ -97,7 +102,7 @@ def run_cnn():
 
 if __name__ == '__main__':
     # run_autoencoder()
-    # run_feedforward()
-    run_feedforward_gradient_checking()
+    run_feedforward()
+    # run_feedforward_gradient_checking()
     # run_cnn()
 

network.py
@@ -65,7 +65,7 @@ class Network():
                     minibatch_x = x[:, idx:]
                     minibatch_y = y[:, idx:]
                 output = self.train_predict(minibatch_x)
-                loss = self.loss(output, minibatch_y).sum()
+                loss = self.loss(output, minibatch_y).mean()
                 self.training_loss.append((epoch, loss))
 
                 if verbose:
@@ -103,7 +103,7 @@ class Network():
             plt.show()
 
     def _check_gradient(self, x, y, layer):
-        epsilon = 1e-5
+        epsilon = 1e-10
         for i in tqdm(range(layer.shape[0])):
             for j in range(layer.shape[1]):
                 layer.weights[i,j] += epsilon
@@ -115,7 +115,9 @@ class Network():
                 raw_gradient = ((loss_plus - loss_minus)/(2*epsilon))
                 gradient = ((loss_plus - loss_minus)/(2*epsilon)).sum()
                 backprop_gradient = layer.gradient[i,j]
-                if not np.isclose(backprop_gradient, gradient, rtol=1):
+                grad_rel_diff = (gradient-backprop_gradient)/(np.absolute(gradient) + np.absolute(backprop_gradient) + 1)
+                # if not np.isclose(grad_rel_diff, 0.0, rtol=1e-5):
+                if not grad_rel_diff < 1e-5:
                     raise Exception(f"Computed gradient is not correct for layer {layer}")
                 # Reset weights
                 layer.weights[i,j] += epsilon
@@ -129,8 +131,10 @@ class Network():
                 loss_minus = self.loss(output_minus, y)
                 raw_gradient = ((loss_plus - loss_minus)/(2*epsilon))
                 gradient = ((loss_plus - loss_minus)/(2*epsilon)).sum()
-                backprop_gradient = layer.bias_gradient[i]
-                if not np.isclose(backprop_gradient, gradient, rtol=1e-5):
+                backprop_gradient = layer.bias_gradient[i].sum()
+                grad_rel_diff = (gradient-backprop_gradient)/(np.absolute(gradient) + np.absolute(backprop_gradient) + 1)
+                # if not np.isclose(grad_rel_diff, 0.0, rtol=1e-5):
+                if not grad_rel_diff < 1e-5:
                     raise Exception(f"Computed gradient is not correct for bias in layer {layer}")
                 layer.bias[i,0] += epsilon
         logging.info(f'All computed gradients are correct for layer {layer}')