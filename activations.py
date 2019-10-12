Fix softmax and crossentropy, continue batchnorm

activations.py
@@ -8,7 +8,8 @@ import numpy as np
 class Activation():
     '''
     Base class for an activation layer
-    Inspired by https://github.com/eriklindernoren/ML-From-Scratch/blob/d8d86600be48117bb4d82f1f60c2d663248c0398/mlfromscratch/deep_learning/activation_functions.py
+    Inspired by 
+    https://github.com/eriklindernoren/ML-From-Scratch/blob/master/mlfromscratch/deep_learning/activation_functions.py
     '''
 
     def __call__(self, x):
@@ -63,8 +64,11 @@ class Sigmoid(Activation):
 class Softmax(Activation):
 
     def __call__(self, x):
-        return np.exp(x) / np.exp(x).sum(axis=0, keepdims=True)
+        # Using normalised x for numerical stability
+        norm_x = x - np.max(x, axis=0)
+        return np.exp(norm_x) / np.exp(norm_x).sum(axis=0, keepdims=True)
 
     def derivative(self, x):
-        # Softmax derivative not used when CrossEntropy loss is used
-        return
+        norm_x = x - np.max(x,axis=0)
+        return self.__call__(norm_x) * (1 - self.__call__(norm_x))
+        

layers.py
@@ -72,7 +72,7 @@ class Layer():
         if self.batch_normalization:
             self._apply_batch_normalization(runtime)
             # Apply activation function to normalized output
-            self.out = self.activation(self.x_hat)
+            self.out = self.activation(self.y)
         else:
             # No batch normalization applied
             if self.use_bias:
@@ -106,16 +106,16 @@ class Layer():
             # Collect mean and variance for later use in testing
             self.mean_wx_list.append(self.mean_wx)
             self.var_wx_list.append(self.var_wx)
-            # Get normalised wx
-            self.norm_wx = (self.wx - self.mean_wx) / (np.sqrt(self.var_wx) + 1e-8)
+            # Get normalised wx - Using var names from batchnorm paper
+            self.x_hat = (self.wx - self.mean_wx) / (np.sqrt(self.var_wx) + 1e-8)
             # Multiply by alpha and add beta parameter
-            self.x_hat = self.batchnorm_gamma * self.norm_wx + self.batchnorm_beta
+            self.y = self.batchnorm_gamma * self.x_hat + self.batchnorm_beta
         else:
             # During inference use population estimators
             self.mean_wx = np.mean(self.mean_wx_list)
             self.var_wx = np.mean(self.var_wx_list)
-            self.norm_wx = (self.wx - self.mean_wx) / (np.sqrt(self.var_wx) + 1e-8)
-            self.x_hat = self.batchnorm_gamma * self.norm_wx + self.batchnorm_beta
+            self.x_hat = (self.wx - self.mean_wx) / (np.sqrt(self.var_wx) + 1e-8)
+            self.y = self.batchnorm_gamma * self.x_hat + self.batchnorm_beta
         return
 
     def update_weights(self, learning_rate, batch_size):
@@ -141,13 +141,21 @@ class Layer():
         self.batchnorm_beta = self.batchnorm_optimizer.update_weights(
                 self.batchnorm_beta, learning_rate, batch_size, self.batchnorm_beta_gradient
                 )
-        # self.gradient = 0
-        # self.bias_gradient = 0
-        # self.error = 0
+        # Update layer error
+        a = self.batchnorm_gamma / (batch_size * (np.sqrt(self.var_wx) + 1e-8))
+        b = batch_size * self.next_layer.error 
+        c = self.next_layer.error.sum(axis=1, keepdims=True)
+        d = (self.wx - self.mean_wx)/(self.var_wx + 1e-8)
+        e = (self.next_layer.error * (self.wx - self.mean_wx)).sum( axis=1, keepdims=True)
+        # This is dL/dwx
+        dldwx = a * (b - c  - d * e)
+        self.error = 0
+        # Not updating bias gradient since bias is not used 
+        _set_gradient()
         return
     
     def _set_batchnorm_gradients(self):
-        self.batchnorm_gamma_gradient = self.error.dot(self.norm_wx.T).sum(axis=1, keepdims=True)
+        self.batchnorm_gamma_gradient = self.error.dot(self.x_hat.T).sum(axis=1, keepdims=True)
         self.batchnorm_beta_gradient = self.error.sum(axis=1, keepdims=True)
         return
     
@@ -171,6 +179,7 @@ class Layer():
         dwx = self.activation.derivative(self.wx)
         if self.add_dropout:
             dwx *= self.dropout_mask
+        # Error <- next layer error * dwx
         self.error = self.next_layer.weights.T.dot(self.next_layer.error) * dwx
         return
 

losses.py
@@ -12,10 +12,9 @@ class MSE(Loss):
     def output_gradient(self, predicted, labels):
         return predicted - labels
 
-
 class CrossEntropy(Loss):
     def __call__(self, predicted, labels):
         return -np.nan_to_num((labels*np.log(predicted) + (1-labels)*np.log(1-predicted)))
 
     def output_gradient(self, predicted, labels):
-        return predicted - labels
+        return np.nan_to_num(-(labels / predicted) + (1 - labels) / (1 - predicted))

main.py
@@ -273,6 +273,25 @@ def run_ff_with_adam():
     get_accuracy_mnist(x_test, y_test, net)
     plot_weights(net)
 
+def run_ff_with_softmax():
+    x_train, y_train, x_test, y_test = load_mnist(train_set_size=10000, test_set_size=1000)
+    ins = 784
+    os = 10
+    hs = 100
+    bs = 1000
+    loss = CrossEntropy()
+    net = Network()
+    net.set_name('FF with Adam')
+    net.add(Feedforward(shape=(hs, ins)))
+    net.add(Output(shape=(os, hs), activation='softmax'))
+    net.train(x_train,  y_train, loss,
+            optimizer='adam', batch_size=bs,
+            learning_rate=1e-4, epochs=100,
+            plot_loss=True, regularizer=('L2', 0.5),
+            )
+    get_accuracy_mnist(x_test, y_test, net)
+    plot_weights(net)
+
 def run_ff_with_batchnorm():
     ins = 784
     os = 10
@@ -294,28 +313,6 @@ def run_ff_with_batchnorm():
     get_accuracy_mnist(x_test, y_test, net)
     # plot_weights(net)
 
-def save_weights_test():
-    # x_train, y_train, x_test, y_test = load_mnist(train_set_size=1000, test_set_size=100)
-    ins = 2
-    os = 10
-    hs = 3
-    bs = 2
-    train_set_size = bs*10
-    x_train = np.random.rand(ins, train_set_size)
-    x_test = np.random.rand(ins, bs)
-    y_train = np.ones((os, train_set_size))
-    y_test = np.ones((os, bs))
-    loss = CrossEntropy()
-    net = Network()
-    net.set_name('Save weights test')
-    net.add(Feedforward(shape=(hs, ins)))
-    net.add(Output(shape=(os, hs)))
-    net.train(x_train,  y_train, loss,
-            optimizer='adam', batch_size=bs,
-            learning_rate=1e-2, epochs=5,
-            save_weights=True
-            )
-
 if __name__ == '__main__':
     # run_autoencoder()
     # run_feedforward()
@@ -332,8 +329,8 @@ if __name__ == '__main__':
     # run_ff_with_adagrad()
     # run_ff_with_rmsprop()
     # run_ff_with_adam()
-    # save_weights_test()
-    run_ff_with_batchnorm()
+    # run_ff_with_softmax()
+    # run_ff_with_batchnorm()
 
 
 