Fix batchorm backprop

layers.py
@@ -103,18 +103,18 @@ class Layer():
             # Mean and variance are estimated over batches
             self.mean_wx = self.wx.mean(axis=1, keepdims=True)
             self.var_wx = ((self.wx - self.mean_wx)**2).mean(axis=1, keepdims=True)
-            # Collect mean and variance for later use in testing
+            # Keep running average of mean and variance for use in testing
             self.mean_wx_avg += self.mean_wx
             self.mean_wx_avg /= 2
             self.var_wx_avg += self.var_wx
             self.var_wx_avg /= 2
             # Get normalised wx - Using var names from batchnorm paper
-            self.x_hat = (self.wx - self.mean_wx) / (np.sqrt(self.var_wx) + 1e-8)
+            self.x_hat = (self.wx - self.mean_wx) / (np.sqrt(self.var_wx) + 1e-12)
             # Multiply by alpha and add beta parameter
             self.y = self.batchnorm_gamma * self.x_hat + self.batchnorm_beta
         else:
             # During inference use population estimators
-            self.x_hat = (self.wx - self.mean_wx_avg) / (np.sqrt(self.var_wx_avg) + 1e-8)
+            self.x_hat = (self.wx - self.mean_wx_avg) / (np.sqrt(self.var_wx_avg) + 1e-12)
             self.y = self.batchnorm_gamma * self.x_hat + self.batchnorm_beta
         return
 
@@ -142,19 +142,11 @@ class Layer():
                 self.batchnorm_beta, learning_rate, batch_size, self.batchnorm_beta_gradient
                 )
         # Update layer error
-        # dxhat = self.batchnorm_gamma * self.next_layer.error
-        inverse_std = 1. / (np.sqrt(self.var_wx) + 1e-8)
-        # self.error = (1. / batch_size) * inverse_std * (
-        #             batch_size * dxhat - dxhat.sum(axis=1, keepdims=True) -
-        #             self.xhat * (dxhat * self.xhat).sum(axis=1, keepdims=True)
-        #         )
+        inverse_std = 1. / (np.sqrt(self.var_wx) + 1e-12)
         a = 1. / batch_size  * self.batchnorm_gamma * inverse_std
-        dLdgamma = self.error.dot(self.x_hat.T).sum(axis=1, keepdims=True)
-        dLdbeta = self.error.sum(axis=1, keepdims=True)
-        self.error = a * ( - dLdgamma * self.x_hat + batch_size * 
-                    self.error.sum(axis=1, keepdims=True) - dLdbeta
+        self.error = a * ( - self.batchnorm_gamma_gradient * self.x_hat 
+                    + batch_size * self.error - self.batchnorm_beta_gradient
                     )
-        
         # Not updating bias gradient since bias is not used 
         self._set_gradient()
         return
@@ -184,7 +176,6 @@ class Layer():
         dwx = self.activation.derivative(self.wx)
         if self.add_dropout:
             dwx *= self.dropout_mask
-        # Error <- next layer error * dwx
         self.error = self.next_layer.weights.T.dot(self.next_layer.error) * dwx
         return
 

optimizers.py
@@ -75,12 +75,12 @@ class Adagrad(Optimizer):
         self.bias_cache = 0
         return
 
-    def update_weights(self, weights, lr, batch_size, gradient, eps=1e-6):
+    def update_weights(self, weights, lr, batch_size, gradient, eps=1e-12):
         self.cache += gradient ** 2
         weights -= lr / (np.sqrt(self.cache) + eps) * gradient
         return weights
 
-    def update_bias(self, bias, lr, batch_size, bias_gradient, eps=1e-6):
+    def update_bias(self, bias, lr, batch_size, bias_gradient, eps=1e-12):
         self.bias_cache += bias_gradient ** 2
         bias -= lr / (np.sqrt(self.bias_cache) + eps) * bias_gradient
         return bias
@@ -92,12 +92,12 @@ class Rmsprop(Optimizer):
         self.bias_cache = 0
         return
 
-    def update_weights(self, weights, lr, batch_size, gradient, eps=1e-6, decay_rate=0.99):
+    def update_weights(self, weights, lr, batch_size, gradient, eps=1e-12, decay_rate=0.99):
         self.cache = decay_rate * self.cache + (1 - decay_rate) * (gradient ** 2)
         weights -= lr / (np.sqrt(self.cache) + eps) * gradient
         return weights
 
-    def update_bias(self, bias, lr, batch_size, bias_gradient, eps=1e-6, decay_rate=0.99):
+    def update_bias(self, bias, lr, batch_size, bias_gradient, eps=1e-12, decay_rate=0.99):
         self.bias_cache = decay_rate * self.bias_cache + (1 - decay_rate) * (bias_gradient ** 2)
         bias -= lr / (np.sqrt(self.bias_cache) + eps) * bias_gradient
         return bias
@@ -112,7 +112,7 @@ class Adam(Optimizer):
         self.bias_cache_v = 0
         return
 
-    def update_weights(self, weights, lr, batch_size, gradient, eps=1e-8, beta1=0.9, beta2=0.999):
+    def update_weights(self, weights, lr, batch_size, gradient, eps=1e-12, beta1=0.9, beta2=0.999):
         self.t += 1
         self.cache_m = beta1*self.cache_m + (1-beta1)*gradient
         # Bias correction
@@ -122,7 +122,7 @@ class Adam(Optimizer):
         weights -= lr * m_corrected / (np.sqrt(v_corrected) + eps)
         return weights
 
-    def update_bias(self, bias, lr, batch_size, bias_gradient, eps=1e-8, beta1=0.9, beta2=0.999):
+    def update_bias(self, bias, lr, batch_size, bias_gradient, eps=1e-12, beta1=0.9, beta2=0.999):
         self.bias_cache_m = beta1*self.bias_cache_m + (1-beta1)*bias_gradient
         # Bias correction
         m_corrected = self.bias_cache_m / (1-beta1**self.t)